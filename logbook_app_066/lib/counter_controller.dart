437680e77f07c0dd5dc125d2acf202e9487158ba
aruda <aldpj27@gmail.com>
2026-02-10 10:48:44 +0700

    menyelesaikan task 1 multi step counter

--- logbook_app_066/lib/counter_controller.dart
+++ logbook_app_066/lib/counter_controller.dart
@@ -1,9 +1,22 @@
 class CounterController {
   int _counter = 0; // Variabel private (Enkapsulasi)
+  int _step = 1; //default stepnya
 
-  int get value => _counter; // Getter untuk akses data
+  int get value => _counter; // Getter untuk akses data counter
+  int get step => _step; // Getter untuk akses data step
 
-  void increment() => _counter++;
-  void decrement() { if (_counter > 0) _counter--; }
+  void setStep(int newStep) // setter step baru
+  {
+    if (newStep > 0) 
+    {
+      _step = newStep;
+    }
+  }
+
+  void increment() => _counter += _step;
+  void decrement() {
+    if (_counter - _step >= 0) _counter -= _step;
+  }
+
   void reset() => _counter = 0;
 }
--- logbook_app_066/lib/counter_view.dart
+++ logbook_app_066/lib/counter_view.dart
@@ -9,23 +9,73 @@ class CounterView extends StatefulWidget {
 
 class _CounterViewState extends State<CounterView> {
   final CounterController _controller = CounterController();
+  final TextEditingController _stepInput = TextEditingController(text: "1");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("LogBook: Versi SRP")),
-      body: Center(
+      appBar: AppBar(title: const Text("Multi step Counter")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text("Total Hitungan:"),
             Text('${_controller.value}', style: const TextStyle(fontSize: 40)),
+
+            const SizedBox(height: 20),
+
+            TextField(
+              controller: _stepInput,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: "Step",
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                final step = int.tryParse(value) ?? 1;
+                setState(() => _controller.setStep(step));
+              },
+            ),
+
+            const SizedBox(height: 20),
+
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     ElevatedButton(
+            //       onPressed: () => setState(() => _controller.decrement()),
+            //       child: const Text("-"),
+            //     ),
+            //     const SizedBox(width: 20),
+            //     ElevatedButton(
+            //       onPressed: () => setState(() => _controller.increment()),
+            //       child: const Text("+"),
+            //     ),
+            //   ],
+            // ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _controller.increment()),
-        child: const Icon(Icons.add),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () => setState(() => _controller.increment()),
+      //   child: const Icon(Icons.add),
+      // ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          FloatingActionButton(
+            heroTag: "decrement",
+            onPressed: () => setState(() => _controller.decrement()),
+            child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            heroTag: "increment",
+            onPressed: () => setState(() => _controller.increment()),
+            child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
